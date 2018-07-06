b070cb3904d40e7b1804446e8a66584f945878a7
yukihane <yukihane.feather@gmail.com>
2018-07-07 05:14:11 +0900

    Servlet Extensions

---
+++ src/main/java/com/github/yukihane/java/undertow/HelloWorldHandler.java
@@ -0,0 +1,15 @@
+package com.github.yukihane.java.undertow;
+
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
+
+public class HelloWorldHandler implements HttpHandler {
+
+    @Override
+    public void handleRequest(HttpServerExchange exchange) throws Exception {
+        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+        exchange.getResponseSender().send("Hello World");
+    }
+
+}
