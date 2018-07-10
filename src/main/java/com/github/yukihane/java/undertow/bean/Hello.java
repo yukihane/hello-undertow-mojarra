38f91527bdd2282bf1f12cccee52e68425894716
yukihane <yukihane.feather@gmail.com>
2018-07-10 23:02:04 +0900

    ボタン実装

--- src/main/java/com/github/yukihane/java/undertow/bean/Hello.java
+++ src/main/java/com/github/yukihane/java/undertow/bean/Hello.java
@@ -1,13 +1,20 @@
 package com.github.yukihane.java.undertow.bean;
 
+import java.util.Date;
 import javax.faces.bean.ManagedBean;
+import javax.faces.event.ActionEvent;
 
 @ManagedBean
 public class Hello {
-    final String world = "Hello World!";
+    private String world = "Hello World!";
 
     public String getWorld() {
         return world;
+    }
+
+    public void updateDate(final ActionEvent event) {
+        final Date now = new Date();
+        world = now.toString();
     }
 
 }
